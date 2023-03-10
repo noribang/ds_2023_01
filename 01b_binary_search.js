3708fb9a0e56c5cb2bc3d5696f3a01f72c042c32
hoonkim <hoonk9387@gmail.com>
2023-03-09 20:31:30 -0600

    Declare and initialize min and max variables.

--- 01b_binary_search.js
+++ 01b_binary_search.js
@@ -1,5 +1,14 @@
 // Log(n) time complexity.
 // Sorted array necessary.
 function binarySearch(arr, val) {
+    // Variable for min value. Initial value at index 0.
+    let min = 0;
+    // Variable for max value. Initial value at last index of array.
+    let max = arr.length - 1;
+    // Loop through array. Keep halving subset of array.
+    while() {
+        
+    }
+
 
 }
