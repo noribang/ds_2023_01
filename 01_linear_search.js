fa2259d26782f38d7eab67ca43d57ccdfd0dda58
hoonkim <hoonk9387@gmail.com>
2023-03-15 21:29:49 -0500

    Started initial binary search comments.

--- 01_linear_search.js
+++ 01_linear_search.js
@@ -1,16 +1,22 @@
-// O(n) time complexity.
-// Need sorted array.
+// O(n) linear time complexity.
+// Requires sorted array.
+
+// Create a function that takes an array and a value.
+// Find the value in the array.
+// Iterate through each element in the array starting 
+// from index position 0 in the array until the value is found.
+// If the value is found during iteration 
+// return the index position.
+// If value is not found return -1.
 function linearSearch(arr, val) {
     for (let i = 0; i < arr.length; i++) {
-        // If element at i position equals val return i.
+        console.log(`Is ${arr[i]} equal to ${val}: `+ arr[i] === val)
         if (arr[i] === val) {
             return i;
         }
     }
-    // If val does not exist in arr return -1.
+    // If value not found return -1.
     return -1;
 }
 
-
-let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
-linearSearch(arrSearch, 46)
+linearSearch([1,2,3,5,6,8,9,12,15,16,29], 2)
