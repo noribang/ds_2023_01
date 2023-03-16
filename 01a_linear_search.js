20daac888aeead66147f2f08d05210608b83a1e2
hoonkim <hoonk9387@gmail.com>
2023-03-15 21:26:56 -0500

    Added for loop and if condition to test for value in array.

--- 01a_linear_search.js
+++ 01a_linear_search.js
@@ -3,13 +3,20 @@
 
 // Create a function that takes an array and a value.
 // Find the value in the array.
-// Iterate through each element starting from index 
-// position 0 in the array until the value is found.
+// Iterate through each element in the array starting 
+// from index position 0 in the array until the value is found.
 // If the value is found during iteration 
 // return the index position.
 // If value is not found return -1.
 function linearSearch(arr, val) {
-
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`Is ${arr[i]} equal to ${val}: `+ arr[i] === val)
+        if (arr[i] === val) {
+            return i;
+        }
+    }
+    // If value not found return -1.
+    return -1;
 }
 
 linearSearch([1,2,3,5,6,8,9,12,15,16,29], 2)
