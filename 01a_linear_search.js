99fdfa2a0d59765b963a5685aeb4b6da22766de7
hoonkim <hoonk9387@gmail.com>
2023-03-15 21:12:20 -0500

    Added pseudocode that describes what the function should do.

--- 01a_linear_search.js
+++ 01a_linear_search.js
@@ -1,8 +1,15 @@
 // O(n) linear time complexity.
 // Requires sorted array.
 
+// Create a function that takes an array and a value.
+// Find the value in the array.
+// Iterate through each element starting from index 
+// position 0 in the array until the value is found.
+// If the value is found during iteration 
+// return the index position.
+// If value is not found return -1.
 function linearSearch(arr, val) {
-    
+
 }
 
 linearSearch([1,2,3,5,6,8,9,12,15,16,29], 2)
