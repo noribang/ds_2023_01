d815e805811fce16d60724983e5fde9f765114e4
hoonkim <hoonk9387@gmail.com>
2023-03-15 21:34:57 -0500

    Added pseudocode of what binary search function should do.

--- 01b_binary_search.js
+++ 01b_binary_search.js
@@ -1,2 +1,19 @@
 // Log(N) time complexity.
 // Requires sorted array or collection.
+
+// Loop through array to find value.
+// Each iteration of array will cut in 
+// half the part of the array being looked at. 
+// Create variables for min position in 
+// array and max position in array.
+
+function binarySearch(arr, val) {
+
+}
+
+
+let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
+binarySearch(arrSearch, 46)
+// binarySearch([1,2,3,4,5,6], 4)
+// binarySearch([1,2,3,4,5,6], 6)
+// binarySearch([1,2,3,4,5,6], 11)
