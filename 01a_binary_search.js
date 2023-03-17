3bd499d586177ce0ca53cbc6bd1cb66ea226672b
hoonkim <hoonk9387@gmail.com>
2023-03-16 21:02:07 -0500

    Added min and max variables for array positions. Started while loop.

--- 01a_binary_search.js
+++ 01a_binary_search.js
@@ -1,6 +1,16 @@
 // Log(N) time complexity.
 // Sorted array or sorted collection required.
 function binarySearch(arr, val) {
+    // Create minimum initial value as index 0.
+    let min = 0;
+    // Create maximum initial value as last index position.
+    let max = arr.length - 1;
+    // Iterate through array.
+    // Use middle as mid position in subset of array.
+    // Check if middle is > or < than value looking for. 
+    while () {
+        
+    }
 
-    
+
 }
