050def952a49e759dc43c71b2de5ac4ff73f5bb8
hoonkim <hoonk9387@gmail.com>
2023-03-16 21:22:43 -0500

    Added test function call.

--- 01a_binary_search.js
+++ 01a_binary_search.js
@@ -34,3 +34,9 @@ function binarySearch(arr, val) {
     // If value not found in array return -1.
     return -1;
 }
+
+let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
+binarySearch(arrSearch, 46)
+// binarySearch([1,2,3,4,5,6], 4)
+// binarySearch([1,2,3,4,5,6], 6)
+// binarySearch([1,2,3,4,5,6], 11)
