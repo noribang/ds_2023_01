51857b36f3cb3d3573d884d1bec31485e78f41d8
hoonkim <hoonk9387@gmail.com>
2023-03-20 05:54:54 -0500

    Added if condition to test if value is found in array.

--- 01a_linear_search.js
+++ 01a_linear_search.js
@@ -6,7 +6,11 @@ function linearSearch(arr, val) {
     // Iterate through a collection of data.
     for (let i = 0; i < arr.length; i++) {
         let currentValue = arr[i]
-        
+        // If current value in arr is val we 
+        // are looking for return i position.
+        if (currentValue === val) {
+            return i;
+        }
     }
 
 }
