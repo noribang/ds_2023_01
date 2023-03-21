74ee729fbddf4b8a6454c02f438a726c720e0873
hoonkim <hoonk9387@gmail.com>
2023-03-20 20:02:27 -0500

    Started function declaration for binary search.

--- 01a_binary_search.js
+++ 01a_binary_search.js
@@ -1,39 +1,10 @@
-// Log(N) time complexity.
-// Sorted array or sorted collection required.
-function binarySearch(arr, val) {
-    // Create minimum initial value as index 0.
-    let min = 0;
-    // Create maximum initial value as last index position.
-    let max = arr.length - 1;
-    // Iterate through array.
-    // Use middle as mid position in subset of array.
-    // Check if middle is > or < than value looking for. 
-    while (min <= max) {
-        // Get middle position in subset of array.
-        let middle = Math.floor((min + max) / 2)
-        // Get value of middle posiiton in array.
-        let currentElement = arr[middle]
-        // If value at middle position < value looking for
-        // make min +1 of current middle (move new min to right of middle).
-        if (currentElement < val) {
-            min =  middle + 1;
-        }
-        // If value at middle position > value looking for 
-        // make max middle -1 (move new max to left of middle).
-        else if (currentElement > val) {
-            max = middle - 1
-        } 
-        // If middle is neither < or > value looking for 
-        // it must be the value looking for.
-        // Return middle position.
-        else {
-            return middle;
-        }
-    }
-    // Default:
-    // If value not found in array return -1.
-    return -1;
+// O(log n) time complexity.
+// Sorted collection needed.
+
+function binarySearch() {
+    
 }
+
 
 let arrSearch = [1,2,3,6,9,12,21,28,35,46,53,63]
 binarySearch(arrSearch, 46)
