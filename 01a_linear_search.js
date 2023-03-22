5c194193c6b581ec7942f5992a0b2c3d33037643
hoonkim <hoonk9387@gmail.com>
2023-03-21 21:40:13 -0500

    Added two parameters.

--- 01a_linear_search.js
+++ 01a_linear_search.js
@@ -1,8 +1,9 @@
 // O(n) linear time complexity.
 // Sorted array required.
 
-function linearSearch() {
-    
+// Search for val in collection.
+function linearSearch(arr, val) {
+
 }
 
 linearSearch([1,2,3,5,6,8,9,12,15,16,29], 2)
