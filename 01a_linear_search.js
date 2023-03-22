52b420558602d3e6668d11a1b1ef8e8a76bc3bd4
hoonkim <hoonk9387@gmail.com>
2023-03-21 21:47:04 -0500

    Added if condition to test if current position value equals val looking for.

--- 01a_linear_search.js
+++ 01a_linear_search.js
@@ -5,14 +5,15 @@
 function linearSearch(arr, val) {
     // Iterate through collection.
     for (let i = 0; i < arr.length; i++) {
-
+        // Check if value at current position 
+        // in collection equals val.
+        // Return current position if equals val.
+        let currentValue = arr[i]    
+        if (currentValue === val) {
+            return i
+        }
     }
     
-    // Check if value at current position 
-    // in collection equals val.
-    // Return current position if equals val.
-
-
 }
 
 linearSearch([1,2,3,5,6,8,9,12,15,16,29], 2)
