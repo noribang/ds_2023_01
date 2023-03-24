571805e8dceb4443c28670d68b8f3572752b7b21
hoonkim <hoonk9387@gmail.com>
2023-03-23 21:27:03 -0500

    Test if two arrays are not equal length.

--- 02a_frequency_counter.js
+++ 02a_frequency_counter.js
@@ -1,6 +1,12 @@
 // O(n^2) quadratic time complexity.
 // Nested loops.
 
+// Test if two collections have the same 
+// values and same frequency of values.
 function same(arr1, arr2) {
+    // Check if collections are not equal length.
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
 
 }
