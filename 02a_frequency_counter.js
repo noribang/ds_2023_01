065ff116345c060ee635e674cda865512e7fa6d8
hoonkim <hoonk9387@gmail.com>
2023-03-23 21:36:40 -0500

    Started inner for loop.

--- 02a_frequency_counter.js
+++ 02a_frequency_counter.js
@@ -10,7 +10,22 @@ function same(arr1, arr2) {
     }
     // Loop through arr1.
     for (let i = 0; i < arr1.length; i++) {
-        
+        let arr1Val = arr1[i]
+        // Loop through arr2. Check each element to arr1Val.
+        for (let j = 0; j < arr2.length; j++) {
+            // If arr2[j] equal to arr1Val.
+            // Remove arr2[j] from arr2.
+            if (arr1Val === arr2[j]) {
+                arr2.splice(j, 1)
+            }
+        }
+
+
     }
 
 }
+
+test1 = [1,2,3,4,8]
+// test2 = [9,25,16,4,1]
+test2 = [8,3,2,1,4]
+same(test1, test2)
