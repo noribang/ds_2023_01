851b39a845c23d0f75a74707c4224e5355a578c8
hoonkim <hoonk9387@gmail.com>
2023-03-27 21:10:55 -0500

    Started comments for frequency counter function.

--- 02a_frequency_counter.js
+++ 02a_frequency_counter.js
@@ -1,38 +1,10 @@
-// O(n^2) quadratic time complexity.
-// Nested loops.
+// O(n^2) quadratic time complexity because of nested loop
+// Check if two collections have same values 
+// and same frequency of values. 
 
-// Test if two collections have the same 
-// values and same frequency of values.
-function same(arr1, arr2) {
-    // Check if collections are not equal length.
-    if (arr1.length !== arr2.length) {
-        return false;
-    }
-    // Loop through arr1.
-    for (let i = 0; i < arr1.length; i++) {
-        let arr1Val = arr1[i]
-        // Loop through arr2. Check each element to arr1Val.
-        for (let j = 0; j < arr2.length; j++) {
-            // If arr2[j] equal to arr1Val.
-            // Remove arr2[j] from arr2.
-            if (arr1Val === arr2[j]) {
-                arr2.splice(j, 1)
-            }
-        }
-    }
-    // If after looping through both arrays
-    // arr2 should be empty. Meaning arr2.length equal 0.
-    // If arr2.length is not equal 0 then 
-    // both arrays do not have the same values and frequencies.
-    // Return false.
-    if (arr2.length !== 0 ) {
-        return false;
-    }
-    // Default:
-    // Return true. arr2.length equals 0.
-    // Both arrays have same values and frequencies of values.
-    return true;
-}
+// Take in two collections.
+// Check if two collections are not of equal length. 
+
 
 test1 = [1,2,3,4,8]
 // test2 = [9,25,16,4,1]
