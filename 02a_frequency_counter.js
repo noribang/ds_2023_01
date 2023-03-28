a46f7f7c819655e869502b7594f6809a1821a975
hoonkim <hoonk9387@gmail.com>
2023-03-27 21:18:55 -0500

    Added pseudo code to describe frequency function.

--- 02a_frequency_counter.js
+++ 02a_frequency_counter.js
@@ -3,7 +3,15 @@
 // and same frequency of values. 
 
 // Take in two collections.
-// Check if two collections are not of equal length. 
+// Check if two collections are not of equal length.
+// If not equal length short circuit and return false. 
+// If equal length loop through both collections.
+
+// Loop through arr1. Start from 0 until end of array.
+// Assign each value in array to variable arr1Val.
+// Nested loop thru arr2.
+// Check if each value in arr2 is equal to arr1.
+
 
 
 test1 = [1,2,3,4,8]
