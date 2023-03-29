4eb1bdeeadae6c4b867fdfc2fd68b08fdedc3188
hoonkim <hoonk9387@gmail.com>
2023-03-28 21:34:24 -0500

    Added nested loop to test for frequency of values in both arrays.

--- 02a_frequency_counter.js
+++ 02a_frequency_counter.js
@@ -16,7 +16,35 @@
 // At end of nested loop check if arr2 is length 0.
 // If arr2 length does not equal 0 return false.
 function same(arr1, arr2) {
-
+    // Check if two collections are not of equal length.
+    // If not equal length short circuit and return false. 
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    // If equal length loop through both collections.
+    // Loop through arr1. Start from 0 until end of array.
+    // Assign each value in array to variable arr1Val.
+    // Nested loop thru arr2.
+    // Check if each value in arr2 is equal to arr1.
+    // If arr1Val equals arr2[j] remove that value position from arr2.
+    for (let i = 0; i < arr1.length; i++) {
+        let arr1Val = arr1[i]
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1Val === arr2[j]) {
+                // Remove that element from arr2.
+                arr2.splice(j, 1)
+            }
+        }
+    }
+    // At end of nested loop check if arr2 is length 0.
+    // If arr2 length does not equal 0 return false.
+    if (arr2.length !== 0) {
+        return false;
+    }
+    // If arr2 is length 0 both arrays have same values
+    // and frequency of values.
+    // Return true.
+    return true;
 } 
 
 
