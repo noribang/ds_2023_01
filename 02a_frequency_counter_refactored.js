5ab198b878901be000c3b94e0951ab21da767717
hoonkim <hoonk9387@gmail.com>
2023-04-03 22:48:02 -0500

    Created file for frequency counter function.

---
+++ 02a_frequency_counter_refactored.js
@@ -0,0 +1,13 @@
+// O(n) linear time complexity.
+// Compare two arrays for same values and same frequency of values.
+
+function same(arr1, arr2) {
+
+    
+}
+
+
+
+test1 = [1,2,3,4,5,3]
+test2 = [9,25,16,4,1,9]
+same(test1, test2)
