cd4ef43366b76ae9faaf85cf592956a98315469a
hoonkim <hoonk9387@gmail.com>
2023-04-05 21:54:37 -0500

    Populate second object with elements and their frequencies from arr2.

--- 02a_frequency_counter_refactored.js
+++ 02a_frequency_counter_refactored.js
@@ -16,6 +16,12 @@ function same(arr1, arr2) {
     for (let val of arr1) {
         frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
     }
+    // Populate elements and their frequencies from 
+    // arr2 into frequencyCounter object. 
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter[val] || 0) + 1
+    }
+
 
 }
 
