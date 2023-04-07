519d48a933da5691ad9fad44e1fda11192d22a81
hoonkim <hoonk9387@gmail.com>
2023-04-06 22:05:40 -0500

    Started inner for loop. Test two pointers if sum to zero. Return array with two pointers if true.

--- 03_multiple_pointers.js
+++ 03_multiple_pointers.js
@@ -5,10 +5,19 @@
 // e.g. [-4,-3,-2,-1,0,1,2,5] 
 
 function sumZero(arr) {
+    // First pointer starts at 0.
     // Iterate through arr.
     for (let i = 0; i < arr.length; i++) {
-        
+        // Second pointer starts at i + 1.
+        for (let j = i + 1; j < arr.length; j++) {
+            // Test if first pointer and second pointer 
+            // sum to 0.
+            // If true return array with two pointers.
+            if (arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]]
+            }
 
+        }
     }
 
 }
